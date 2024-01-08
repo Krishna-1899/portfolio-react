9392f89782a53b8ccac332447f9322c107763f9c
Krishna-1899 <rajeshbhanushali189@gmail.com>
2024-01-08 11:36:25 +0530

    f

--- server/route/contact.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-const router = express.Router();
-
-const {getAllWorks,submitResponse} =  require("../controllers/controller");
-// imageUpload,vedioUpload,imageReducerUpload,
-router.get("/getAllWorks",getAllWorks);
-router.post("/submitResponse",submitResponse);
-module.exports=router;
